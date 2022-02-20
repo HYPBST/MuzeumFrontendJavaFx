bb53c983a1e909e10402d50684de9e885a20bbd0
Gergő <s.gergo.az@gmail.com>
2022-02-20 19:58:18 +0100

    Init

---
+++ src/main/java/com/example/muzeumfrontendjavafx/HelloController.java
@@ -0,0 +1,20 @@
+package com.example.muzeumfrontendjavafx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class HelloController {
+
+
+    public void onHozzadasButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void onModositasButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void onTorlesButtonClick(ActionEvent actionEvent) {
+    }
+}
